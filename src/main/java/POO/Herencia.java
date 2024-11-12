2d8294cfd74de69a81cccbe2f1908b0e8b0c3a47
kenk7 <kenk7@KenCastellanos>
2024-11-11 23:08:07 -0600

    prueba clase

---
+++ src/main/java/POO/Herencia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package POO;
+
+/**
+ *
+ * @author kenk7
+ */
+public class Herencia {
+
+    public static void main(String[] args)
+    {
+        Animal p = new perro("negro", "si", "si", "si", 4);
+        p.Sonido();
+    }
+}
